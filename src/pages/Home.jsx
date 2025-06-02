af730172de186b4b9171b7a0094c4629e7c0844c
mohamedo396 <mohamadamrani83@gmail.com>
2025-06-02 15:27:22 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Navbar from '../components/Navbar';
+import ThemeToggle from '../components/ThemeToggle';
+import StartBackground from '../components/StartBackground';
+import HeroSection from '../components/HeroSection';
+import AboutSection from '../components/AboutSection';
+import SkillsSection from '../components/SkillsSection';
+import ProjectsSection from '../components/ProjectsSection';
+import ContactSection from '../components/ContactSection';
+import Footer from '../components/Footer';
+
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
